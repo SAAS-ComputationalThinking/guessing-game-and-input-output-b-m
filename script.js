74c380aaea4b78414ec7021e69f265580538646e
Barok <barokgebre@seattleacademy.org>
2024-04-22 12:01:04 -0700

    we essentially added a way for the computer to check if the guessed answer was correct or incorrect and generated a random number

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-let x = ((Math.random() * 10) + 1);
+function guessNumber(){
+    const random = Math.floow(math.random()+ 10) +1;
+    if (number == x) {
+        console.log("you are correct")
+    } else if( number > x  )
+    console.log("too high")
+    else if( number < x  )
+    console.log("too low")
+
+}
+ 
+
+
